e3f1394bce1d2390de2bdd9f00a71083908a562e
Jay Weight <jaydweight@gmail.com>
2018-05-02 08:07:29 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import SearchField from './SearchField';
+// import PhotoResults from './PhotoResults';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchField />
+        {/* <PhotoResults /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
